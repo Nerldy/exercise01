37ec5dbe382c6839d464e16595fe57e27c7274a1
Noldy PC <arnodhis@gmail.com>
2017-07-19 11:42:31 +0300

    Created a program called Get the Middle Character. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const names = document.querySelectorAll("input");
+// Get the Middle Character
 
 
-for (let i = 0; i < names.length; i++) {
-    const checkName = names[i].value.toUpperCase();
-    if (checkName !== "SUBMIT") {
-        names[i].className = "testing";
-        console.log(checkName);
-        console.log(names[i]);
+
+/*You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.*/
+
+
+
+
+
+function getMiddle(s) {
+
+    const words = s;
+
+    const wordsLength = words.length % 2;
+
+
+
+    if (wordsLength === 0) {
+
+        const middle = words.length / 2;
+
+        return words.substr(middle - 1, 2);
+
+    } else {
+
+        const minusOne = words.length - 1;
+
+        const calculate = minusOne / 2;
+
+        return words.substr(calculate, 1);
+
     }
+
 }
 
-for (let i = 0; i < 10; i++) {
-    const createDivs = document.createElement('div');
-    createDivs.className = "meeko";
-    if (i >= 5) {
-        createDivs.className = "the-fivers";
-    }
-    document.body.appendChild(createDivs);
 
-}
+
+/*Tests*/
+
+console.log(getMiddle('test'));
+
+console.log(getMiddle('testing'));
+
+console.log(getMiddle('middle'));
+
+console.log(getMiddle('A'));
