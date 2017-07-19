023ad3c7c2f53fb973185265d86c1d5d4239130e
Noldy PC <arnodhis@gmail.com>
2017-07-19 16:20:31 +0300

    modified FizzBuzz script to write 'FizzBuzz' for numbers that are divisible by both 3 and 5

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 /*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.*/
 
 for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
     if (i % 3 === 0) {
         console.log('Fizz');
-    } else if (i % 5 === 0 && i % 3 !== 0) {
+    } else if (i % 5 === 0) {
         console.log("Buzz");
     } else {
         console.log(i);
