a0104d592308b7c23b39a448333d51e27e048846
Noldy PC <arnodhis@gmail.com>
2017-07-19 15:54:49 +0300

    created a program called FizzBuzz in scripts.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-/*Looping a triangle*/
+/*FizzBuzz*/
 
-let hash = "#";
+/*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.*/
 
-for (var i = 0; i < 7; i++) {
-    console.log(hash);
-    hash += hash;
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
