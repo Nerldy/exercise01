f03762618d525413a17c26891857470ebf3f95ae
Noldy PC <arnodhis@gmail.com>
2017-07-23 07:53:36 +0300

    Arrays and Oblects exercise by Bianca Gandolfo

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-/*FizzBuzz*/
+// exercise https://github.com/bgando/array-exercises/
+var animal = {
+    username: 'ratatolie',
+    tagline: 'cook rats',
+    noises: ['squeak', 'hiss', 'chomp']
+};
 
-/*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.*/
+var noisearray = [
+    "meeow"
+];
+noisearray.unshift("woof");
+noisearray.push("awoo");
+noisearray[noisearray.length] = 'ssss-sss';
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    }
-    if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
+noisearray.length; // 4
+
+var addA = animal.noises;
+
+addA.push(noisearray);
+
+var animals = [];
+
+animals.push(animal);
+
+var quackers = {username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl']};
+
+animals[1] = quackers;
+
+var goat = {
+    username: "GOAT",
+    tagline: 'chewed like green',
+    noises: ['mee-eeh', 'baa-aah']
+};
+
+var bird = {
+    username: "humm-stalk",
+    tagline: "I tree-eed",
+    noises: ['chirp', 'hum', 'sing']
+};
+
+animals.push(goat);
+animals.push(bird);
+
+console.log(animals.length);
